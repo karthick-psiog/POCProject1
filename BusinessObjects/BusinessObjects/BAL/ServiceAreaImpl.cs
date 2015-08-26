288edc6c6d16f48f75759707e4ee91363cafd038
muthukumar-chandrapaul <muthukumar.chandrapaul@psiog.com>
2015-08-26 19:44:54 +0530

    BAL classes

---
+++ BusinessObjects/BusinessObjects/BAL/ServiceAreaImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.DAL;
+namespace BusinessObjects.BAL
+{
+    class ServiceAreaImpl:IServiceArea,IServiceability
+    {   
+        public ServiceAreaImpl() { }
+        public ServiceAreaImpl(string p) 
+        {
+            Pin = p;
+        }
+
+        public string Pin { get; private set { } }
+
+        List<string> IServiceArea.getServiceAreas()
+        {
+            return DataStore.getServiceAreas();
+        }
+
+        bool IServiceArea.findByPin()
+        {
+            return DataStore.findByPin(Pin);            
+        }
+    }
+}
