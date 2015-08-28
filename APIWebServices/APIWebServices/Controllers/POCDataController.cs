52e95605916fdeb99603c6d6293c270ef724f378
karthick <karthick.ganesan@psiog.com>
2015-08-28 17:01:24 +0530

    OAuth Implementation

---
+++ APIWebServices/APIWebServices/Controllers/POCDataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace APIWebServices.Controllers
+{
+    [RoutePrefix("api/data")]
+    [Authorize]
+    public class POCDataController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            ClaimsPrincipal principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
+            var userName = principal.Claims.Where(c => c.Type == "sub").Single().Value;
+            return Ok("You are allowed to request data");
+        }
+    }
+}
