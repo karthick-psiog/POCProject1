ec6937a140928ba3298fad39f314cb8ee4deb255
karthick <karthick.ganesan@psiog.com>
2015-09-01 16:48:51 +0530

    Webservie - Parameters

--- APIWebServices/APIWebServices/Controllers/POCDataController.cs
+++ APIWebServices/APIWebServices/Controllers/POCDataController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APIWebServices.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,41 +10,47 @@ using System.Web.Http;
 namespace APIWebServices.Controllers
 {
     [RoutePrefix("")]
-    [Authorize]
+    //[Authorize]
     public class POCDataController : ApiController
     {
         [Route("")]
         public IHttpActionResult Get()
         {
-            ClaimsPrincipal principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
-            var userName = principal.Claims.Where(c => c.Type == "sub").Single().Value;
+            //ClaimsPrincipal principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
+            //var userName = principal.Claims.Where(c => c.Type == "sub").Single().Value;
             return Ok("You are allowed to request data");
         }
 
+        [HttpGet()]
         [Route("customer/mobile")]
-        public IHttpActionResult CheckAvailability()
+        public String CheckAvailability([FromUri] String mobile)
         {
-            return Ok("mobile number is available : ");
+            return "mobile number is available : " + mobile.ToString();
         }
 
+        [HttpGet()]
         [Route("servicearea/pin")]
-        public IHttpActionResult CheckServiceability()
+        public String CheckServiceability(String PIN)
         {
-            return Ok("PIN is available");
+            return "PIN is available: " +PIN.ToString();
         }
 
+        [HttpGet()]
         [Route("remotecustomer/mobile")]
-        public IHttpActionResult CheckRemoteAvailability()
+        public IHttpActionResult CheckRemoteAvailability(String mobile)
         {
-            return Ok("Remote Cutomer mobile number is available");
+            return Ok("Remote Cutomer mobile number is available: " + mobile.ToString());
         }
 
+        [HttpPost()]
         [Route("registeruser")]
-        public IHttpActionResult RegisterUser()
+        public IHttpActionResult RegisterUser(User userinfo)
         {
-            return Ok("Successfully Registered");
+            
+            return Ok("Successfully Registered" + userinfo.firstname.ToString());
         }
 
+        [HttpPost()]
         [Route("remotenotify")]
         public IHttpActionResult RemoteNotification()
         {
---
+++ APIWebServices/APIWebServices/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIWebServices.Models
+{
+    public class User
+    {
+        public String mobile { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public String firstname { get; set; }
+
+        public String lastname { get; set; }
+
+        public String address { get; set; }
+
+        public String city { get; set; }
+
+        public String PIN { get; set; }
+
+        public String state { get; set; }
+
+        public String country { get; set; }
+    }
+}
