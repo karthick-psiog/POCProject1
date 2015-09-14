e9d199e747ad376c7fb995c13d94426a5626e4b4
muthukumar-chandrapaul <muthukumar.chandrapaul@psiog.com>
2015-09-14 12:00:26 +0530

    customer registration

---
+++ BusinessObjects/BusinessObjects/BAL/CustomerRegistration.cs
@@ -0,0 +1,34 @@
+﻿using BusinessObjects.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.BAL
+{
+    class CustomerRegistration:IRegistrationService
+    {
+        public bool checkUserExists(string mobile)
+        {
+          return DataStore.checkUserAvailability(mobile);          
+        }
+
+        public RemoteCustomer checkUserRemoteExists(string mobile)
+        {
+           return  DataStore.checkUserRemoteAvailability(mobile);
+
+        }
+
+        public bool registerUser(Customer customer, string password)
+        {
+            return DataStore.saveUser(customer, password);            
+        }
+
+        public  Customer getUser(int userid) 
+        {
+            return DataStore.getUser(userid);
+        }
+
+    }
+}
